17758a779ac941ad6a6029c7af08ab5078aecc2c
dakshayk <akshaykumar.career@gmail.com>
2026-03-11 13:29:26 +0530

    terraform functions practise
    terraform functions practise

---
+++ terrafromfunctionsmain.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+locals {
+  value = "Hello World"
+}
+
+
+variable "string_list" {
+  type = list(string)
+  default = [ "serv1", "serv2", "serv3", "serv1", ]
+}
+
+output "output" {
+  #value = upper(local.value)
+  #value = startswith(local.value, "hello")
+  #value = split(" ", local.value)
+  #value = min(1,2,3,4,5)
+  #value = abs(-15.1234)
+  #value = length(var.string_list)
+  #value = join(":" , var.string_list)
+  #value = contains(var.string_list, "serv1")
+  value = toset(var.string_list)
+}
